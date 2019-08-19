39a433869a3196c67a03b1cbb835da3ade18ef61
hoang48 <tranhuyhoang8496@gmail.com>
2019-08-19 15:56:17 +0700

    Hoang48 first commit

--- fresher_fe_2019-master/adaptis-master/src/app.js
+++ fresher_fe_2019-master/adaptis-master/src/app.js
@@ -1,7 +1,11 @@
 // import './assets/styles/_app.scss';
 import Global from './views/components/global/global';
 import Header from './views/components/header/header';
+// import './views/components/airbnbUpdate/header/header';
+import HeaderAirBnb from './views/components/airbnbUpdate/header/header';
+
 require('normalize.css');
+// require('slick.css');
 
 $(() => {
   // Code here
@@ -9,7 +13,42 @@ $(() => {
 
 $(document).ready(() => {
   Global.lazyLoad();
+  $('.responsive').slick({
+    infinite: false,
+    speed: 300,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ]
+  });
 });
+
+
 
 $(window).on('resize', () => {
   setTimeout(() => {
@@ -20,3 +59,6 @@ $(window).on('resize', () => {
 window.onload = () => {
 
 };
+
+$(window).on('scroll', () => {HeaderAirBnb.scrollFunction();});
+
