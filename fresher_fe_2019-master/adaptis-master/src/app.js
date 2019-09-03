18adb310f97bd6df548c03270818549825556fdd
Thiep <thiepvu98@gmail.com>
2019-09-03 16:31:35 +0700

    Thiep update

--- fresher_fe_2019-master/adaptis-master/src/app.js
+++ fresher_fe_2019-master/adaptis-master/src/app.js
@@ -1,10 +1,13 @@
 // import './assets/styles/_app.scss';
 import Global from './views/components/global/global';
+import header from './views/components/header/header';
 import offersSlide from './views/components/kem-homepage/kem-homepage-offers/kem-homepage-offers';
 import footer from './views/components/footer/footer';
 import footerModal from './views/components/modals/modal-footer/modal-footer';
 import search from './views/components/kem-homepage/kem-search/kem-search';
 import searchModal from './views/components/modals/modal-search/modal-search';
+import destinationPlace from './views/components/kem-destination/kem-destination-place/kem-destination-place';
+
 
 
 require('normalize.css');
@@ -21,6 +24,8 @@ $(document).ready(() => {
   footerModal.init();
   search.init();
   searchModal.init();
+  header.init();
+  destinationPlace.init();
 });
 
 $(window).on('resize', () => {
