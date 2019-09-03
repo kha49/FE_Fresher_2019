f3d23325fecbf203289233687c73023769a4c5d7
hoang48 <tranhuyhoang8496@gmail.com>
2019-09-03 15:58:06 +0700

    hoang

--- fresher_fe_2019-master/adaptis-master/src/app.js
+++ fresher_fe_2019-master/adaptis-master/src/app.js
@@ -2,8 +2,6 @@
 import Global from './views/components/global/global';
 import Header from './views/components/header/header';
 import adaptisSlider from '../src/views/components/adaptis/slide/slide';
-// bootstrap;
-import 'bootstrap';
 // import reveal from '../src/views/components/adaptis/develop/develop';
 require('normalize.css');
 // require(['skrollr'], function(skrollr){
