e32245b67d0628a48d6dbb715c367a97f116962e
hoang48 <tranhuyhoang8496@gmail.com>
2019-09-05 16:56:43 +0700

    ban Hoang pull slide

--- fresher_fe_2019-master/adaptis-master/src/app.js
+++ fresher_fe_2019-master/adaptis-master/src/app.js
@@ -1,6 +1,7 @@
 import Global from './views/components/global/global';
 import load from './views/components/adaptisVer2/adaptisVer2SolutionBlock/adaptisVer2SolutionBlock';
 import adaptisHeaderLogo from './views/components/adaptisVer2/adaptisVer2Header/adaptisVer2Header';
+import adaptisSlide from './views/components/adaptisVer2/adaptisVer2Slide/adaptisVer2Slide';
 import skrollr from '../node_modules/skrollr/src/skrollr';
 
 import '../node_modules/slick-carousel/slick/slick';
@@ -13,12 +14,12 @@ $(() => {
 $(document).ready(() => {
   Global.lazyLoad();
   adaptisHeaderLogo.init();
+  adaptisSlide.init();
   $('.single-item').slick({
     infinite: false,
     speed: 300,
     slidesToShow: 1,
-    slidesToScroll: 1,
-    arrow: true
+    slidesToScroll: 1
   });
   skrollr.init();
   load.init();
