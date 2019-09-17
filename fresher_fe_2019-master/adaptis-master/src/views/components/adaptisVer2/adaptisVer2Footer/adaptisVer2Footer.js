657f4bc9965b08a44f81298e03d6d2e463de7b18
hoang48 <tranhuyhoang8496@gmail.com>
2019-09-17 09:44:49 +0700

    ban Hoang update uppercase function

--- fresher_fe_2019-master/adaptis-master/src/views/components/adaptisVer2/adaptisVer2Footer/adaptisVer2Footer.js
+++ fresher_fe_2019-master/adaptis-master/src/views/components/adaptisVer2/adaptisVer2Footer/adaptisVer2Footer.js
@@ -1,6 +1,9 @@
 const adaptisFooter = {
     init: () => {
         let adaptisFooterItems = document.querySelectorAll('.adaptis-footer-item');
+        let adaptisClientLocation = document.querySelectorAll('.adaptis-footer-clock__text--location');
+        let adaptisClientEmail = document.querySelectorAll('.adaptis-footer-clock__text--email');
+        let adaptisClientArray = [adaptisClientLocation, adaptisClientEmail];     
         let initializationDelay = 0.2;
         for(let i = 0; i <= adaptisFooterItems.length; i++) {
             let adaptisFooterItemChild = document.querySelector(`.adaptis-footer-item:nth-child(${i})`);
@@ -8,6 +11,18 @@ const adaptisFooter = {
                 adaptisFooterItemChild.style = `animation-delay: ${initializationDelay}s`;
                 initializationDelay += 0.2;
             }
+        }
+        if(adaptisClientArray) {
+            for(let i = 0; i < adaptisClientArray.length; i++) {
+                if(adaptisClientArray[i]) {
+                    adaptisFooter.upperCase(adaptisClientArray[i]);
+                }
+            }
+        }
+    },
+    upperCase: (variable) => {
+        for(let i = 0; i < variable.length; i++) {
+            variable[i].style = 'text-transform: uppercase';
         }
     }
 }
